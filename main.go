db43d3cd4f60d90cd8c0f7d75da3a958602998da
E-Code30 <edwardjefta55@gmail.com>
2022-09-24 18:52:28 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type AirConditioner struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type Response struct {
+	Code    string      `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func main() {
+	http.HandleFunc("/api/v1/airconditioner", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("success"))
+	})
+
+	port := "8000"
+	fmt.Println("server run on port", port)
+	http.ListenAndServe(";"+port, nil)
+}
